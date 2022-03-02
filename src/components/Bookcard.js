b4dc6bc078d73968f82c8be648768860add0ceaf
Ali batashi <batashi.ali@gmail.com>
2022-03-02 16:21:28 +0330

    added basic homepage

--- src/components/Bookcard.js
+++ src/components/Bookcard.js
@@ -3,15 +3,17 @@ import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Bookcard({ book }) {
-  return (
-    <Card className="bookCards my-3">
+  return book ? (
+    <Card data-testid="card" className="bookCards my-3">
       <Card.Body className="bookCards">
-        <Card.Title>{book.name}</Card.Title>
-        <Card.Subtitle>{book.id}</Card.Subtitle>
+        <Card.Title data-testid="title">{book.name}</Card.Title>
+        <Card.Subtitle data-testid="subTitle">{book.id}</Card.Subtitle>
         <Card.Link>
           <Link to={`/book/${book.id}`}>More Details</Link>
         </Card.Link>
       </Card.Body>
     </Card>
+  ) : (
+    <div data-testid="noData">Error 500</div>
   );
 }
